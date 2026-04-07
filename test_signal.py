e37947b91394e7dcae603420b8a808ede2b63b0a
Cannon Thomas <cannonthomas@Cannons-MacBook-Pro.local>
2026-04-07 12:29:16 -0500

    updated

--- test_signal.py
+++ test_signal.py
@@ -1,10 +1,10 @@
 import gpiod
 import time
 
-chip = gpiod.Chip('gpiochip4')  # Pi 5 uses gpiochip4
-
 PIN_A = 23
 PIN_B = 24
+
+chip = gpiod.Chip("gpiochip4")
 
 line_a = chip.get_line(PIN_A)
 line_b = chip.get_line(PIN_B)
@@ -12,19 +12,19 @@ line_b = chip.get_line(PIN_B)
 line_a.request(consumer="dcc", type=gpiod.LINE_REQ_DIR_OUT)
 line_b.request(consumer="dcc", type=gpiod.LINE_REQ_DIR_OUT)
 
-print("Starting Pi5 signal test...")
+print("Running signal test... Ctrl+C to stop.")
 
 try:
     while True:
         line_a.set_value(1)
         line_b.set_value(0)
-        time.sleep(0.000058)
+        time.sleep(0.001)
 
         line_a.set_value(0)
         line_b.set_value(1)
-        time.sleep(0.000058)
+        time.sleep(0.001)
 
 except KeyboardInterrupt:
+    print("Stopped")
     line_a.set_value(0)
     line_b.set_value(0)
-    print("Stopped")
