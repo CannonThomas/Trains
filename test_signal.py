e2ab233ec1f81b319da473e4736e61dc13998c49
Cannon Thomas <cannonthomas@Cannons-MacBook-Pro.local>
2026-04-07 13:13:10 -0500

    update

--- test_signal.py
+++ test_signal.py
@@ -1,10 +1,12 @@
 import gpiod
 import time
 
-# Pins
-PIN_A = 23  # IN1
-PIN_B = 24  # IN2
-PIN_EN = 18 # ENA
+# -----------------------
+# GPIO SETUP
+# -----------------------
+PIN_A = 23   # IN1
+PIN_B = 24   # IN2
+PIN_EN = 18  # ENA
 
 chip = gpiod.Chip("gpiochip4")
 
@@ -16,25 +18,54 @@ line_a.request(consumer="dcc", type=gpiod.LINE_REQ_DIR_OUT)
 line_b.request(consumer="dcc", type=gpiod.LINE_REQ_DIR_OUT)
 line_en.request(consumer="dcc", type=gpiod.LINE_REQ_DIR_OUT)
 
-# 🔥 TURN ON H-BRIDGE
+# Enable H-bridge
 line_en.set_value(1)
 
-print("Running signal test...")
+# -----------------------
+# MICROSECOND DELAY
+# -----------------------
+def delay_us(us):
+    start = time.perf_counter()
+    end = start + us / 1_000_000.0
+    while time.perf_counter() < end:
+        pass
+
+# -----------------------
+# DCC BIT GENERATION
+# -----------------------
+def dcc_bit(us):
+    # first half cycle
+    line_a.set_value(1)
+    line_b.set_value(0)
+    delay_us(us)
+
+    # second half cycle (invert)
+    line_a.set_value(0)
+    line_b.set_value(1)
+    delay_us(us)
+
+def send_one():
+    dcc_bit(58)   # DCC "1"
+
+def send_zero():
+    dcc_bit(100)  # DCC "0"
+
+# -----------------------
+# MAIN LOOP (TEST SIGNAL)
+# -----------------------
+print("Running DCC test signal...")
 
 try:
     while True:
-        # polarity A
-        line_a.set_value(1)
-        line_b.set_value(0)
-        time.sleep(0.001)
+        # Preamble (lots of 1s)
+        for _ in range(20):
+            send_one()
 
-        # polarity B
-        line_a.set_value(0)
-        line_b.set_value(1)
-        time.sleep(0.001)
+        # One 0 to separate
+        send_zero()
 
 except KeyboardInterrupt:
-    print("Stopped")
+    print("Stopping...")
     line_a.set_value(0)
     line_b.set_value(0)
     line_en.set_value(0)
