fb047f5b196016c2d738e433e5ca4f731865422b
Cannon Thomas <cannonthomas@Cannons-MacBook-Pro.local>
2026-04-14 11:42:54 -0500

    update

--- dcc_pio.c
+++ dcc_pio.c
@@ -1,56 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #include <gpiod.h>
 
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
-#include "genseq.pio.h"
+#include "dcc_wave.pio.h"
 
-#define PIN_EN   18
-#define PIN_A    23
+#define PIN_EN 18
+#define PIN_A  23   // IN1
+#define PIN_B  24   // IN2
 
-#define DATA_WORDS 1024
-
-#define ONE_US   58
-#define ZERO_US  116
-
-static uint32_t databuf[DATA_WORDS];
-
-static int build_test_stream(void)
-{
-    int idx = 0;
-
-    // Preamble (1 bits)
-    for (int i = 0; i < 200 && idx < DATA_WORDS; i++) {
-        databuf[idx++] = ONE_US;
-    }
-
-    // Some 0 bits
-    for (int i = 0; i < 40 && idx < DATA_WORDS; i++) {
-        databuf[idx++] = ZERO_US;
-    }
-
-    return idx;
-}
+#define ONE_DELAY   58
+#define ZERO_DELAY  116
 
 int main()
 {
-    bool use_dma = true;
-    int ret = 0;
-
     stdio_init_all();
 
     // -----------------------
-    // ENABLE H-BRIDGE (Linux GPIO)
+    // ENABLE H-BRIDGE
     // -----------------------
-    struct gpiod_chip *chip;
-    struct gpiod_line *en_line;
-
-    chip = gpiod_chip_open("/dev/gpiochip0");
-    en_line = gpiod_chip_get_line(chip, PIN_EN);
-
+    struct gpiod_chip *chip = gpiod_chip_open("/dev/gpiochip0");
+    struct gpiod_line *en_line = gpiod_chip_get_line(chip, PIN_EN);
     gpiod_line_request_output(en_line, "dcc", 1);
 
     // -----------------------
@@ -58,57 +31,43 @@ int main()
     // -----------------------
     PIO pio = pio0;
     int sm = pio_claim_unused_sm(pio, true);
-    uint offset = pio_add_program(pio, &genseq_program);
 
-    uint gpio = PIN_A;
+    uint offset = pio_add_program(pio, &dcc_wave_program);
 
-    printf("Loaded program at %d, using sm %d, gpio %d\n", offset, sm, gpio);
+    printf("Loaded DCC program at %d\n", offset);
 
-    pio_sm_config_xfer(pio, sm, PIO_DIR_FROM_SM, 256, 1);
+    // init BOTH pins
+    pio_gpio_init(pio, PIN_A);
+    pio_gpio_init(pio, PIN_B);
 
-    pio_gpio_init(pio, gpio);
-    pio_sm_set_consecutive_pindirs(pio, sm, gpio, 1, true);
+    pio_sm_set_consecutive_pindirs(pio, sm, PIN_A, 2, true);
 
-    pio_sm_config c = genseq_program_get_default_config(offset);
-    sm_config_set_sideset_pins(&c, gpio);
+    pio_sm_config c = dcc_wave_program_get_default_config(offset);
+
+    // IMPORTANT: sideset base pin = PIN_A (covers A + B)
+    sm_config_set_sideset_pins(&c, PIN_A);
+
+    // slow clock for correct timing
+    sm_config_set_clkdiv(&c, 125.0f);  
 
     pio_sm_init(pio, sm, offset, &c);
     pio_sm_set_enabled(pio, sm, true);
 
-    int count = build_test_stream();
-
-    printf("Streaming %d timing words\n", count);
+    printf("Starting DCC waveform...\n");
 
     // -----------------------
-    // MAIN LOOP
+    // LOOP
     // -----------------------
-    while (1) {
-        pio_sm_put_blocking(pio, sm, 0);
+    while (1)
+    {
+        // DCC "1" bit (short)
+        pio_sm_put_blocking(pio, sm, ONE_DELAY);
+        pio_sm_put_blocking(pio, sm, ONE_DELAY);
 
-        if (use_dma) {
-            ret = pio_sm_xfer_data(
-                pio,
-                sm,
-                PIO_DIR_FROM_SM,
-                count * sizeof(databuf[0]),
-                databuf
-            );
-            if (ret) {
-                printf("DMA error %d\n", ret);
-                break;
-            }
-        } else {
-            for (int i = 0; i < count; i++) {
-                pio_sm_put_blocking(pio, sm, databuf[i]);
-            }
-        }
-
-        sleep_ms(5);
+        // DCC "0" bit (long)
+        pio_sm_put_blocking(pio, sm, ZERO_DELAY);
+        pio_sm_put_blocking(pio, sm, ZERO_DELAY);
     }
 
-    // cleanup
-    gpiod_line_set_value(en_line, 0);
-    gpiod_chip_close(chip);
-
-    return ret;
+    return 0;
 }
