05d22169a8cecf5681bb23f74a196ab689f9406d
Cannon Thomas <cannonthomas@Cannons-MacBook-Pro.local>
2026-04-28 08:25:25 -0500

    update

--- dcc_pio.c
+++ dcc_pio.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <sys/select.h>
 #include <gpiod.h>
 
 #include "pico/stdlib.h"
@@ -16,134 +13,62 @@
 #define DCC_ONE_US   58
 #define DCC_ZERO_US 116
 
-#define LOCO_ADDR 3
-#define DCC_INST_128 0x3F
-
-#define MAX_HALVES 1024
-
-static uint32_t wave[MAX_HALVES];
-static int wave_count = 0;
-
-static uint8_t current_speed = 0x80;  // stop forward
+// IDLE packet: FF 00 FF
+#define B0 0xFF
+#define B1 0x00
+#define B2 0xFF
 
 static inline uint32_t pio_count_from_us(uint32_t us) {
     return (us > 2) ? (us - 2) : 1;
 }
 
-static inline void add_bit(int bit) {
-    uint32_t count = bit ? pio_count_from_us(DCC_ONE_US)
-                         : pio_count_from_us(DCC_ZERO_US);
-
-    if (wave_count + 2 < MAX_HALVES) {
-        wave[wave_count++] = count;
-        wave[wave_count++] = count;
-    }
+static inline void send_bit(PIO pio, int sm, int bit,
+                            uint32_t one_count,
+                            uint32_t zero_count) {
+    uint32_t count = bit ? one_count : zero_count;
+    pio_sm_put_blocking(pio, sm, count);
+    pio_sm_put_blocking(pio, sm, count);
 }
 
-static void add_byte(uint8_t b) {
+static void send_byte(PIO pio, int sm, uint8_t b,
+                      uint32_t one_count,
+                      uint32_t zero_count) {
     for (int i = 7; i >= 0; i--) {
-        add_bit((b >> i) & 1);
+        send_bit(pio, sm, (b >> i) & 1, one_count, zero_count);
     }
 }
 
-static void print_packet(const char *label) {
-    uint8_t checksum = LOCO_ADDR ^ DCC_INST_128 ^ current_speed;
+static void send_idle(PIO pio, int sm,
+                      uint32_t one_count,
+                      uint32_t zero_count) {
 
-    printf("%s | speed=0x%02X | packet=%02X %02X %02X %02X\n",
-           label,
-           current_speed,
-           LOCO_ADDR,
-           DCC_INST_128,
-           current_speed,
-           checksum);
-    fflush(stdout);
-}
-
-static void build_packet(void) {
-    wave_count = 0;
-
-    uint8_t checksum = LOCO_ADDR ^ DCC_INST_128 ^ current_speed;
-
+    // Preamble
     for (int i = 0; i < 20; i++) {
-        add_bit(1);
+        send_bit(pio, sm, 1, one_count, zero_count);
     }
 
-    add_bit(0);
-    add_byte(LOCO_ADDR);
+    // FF
+    send_bit(pio, sm, 0, one_count, zero_count);
+    send_byte(pio, sm, B0, one_count, zero_count);
 
-    add_bit(0);
-    add_byte(DCC_INST_128);
+    // 00
+    send_bit(pio, sm, 0, one_count, zero_count);
+    send_byte(pio, sm, B1, one_count, zero_count);
 
-    add_bit(0);
-    add_byte(current_speed);
+    // FF
+    send_bit(pio, sm, 0, one_count, zero_count);
+    send_byte(pio, sm, B2, one_count, zero_count);
 
-    add_bit(0);
-    add_byte(checksum);
-
-    add_bit(1);
-}
-
-static void handle_stdin_command(void) {
-    fd_set rfds;
-    struct timeval tv = {0, 0};
-
-    FD_ZERO(&rfds);
-    FD_SET(STDIN_FILENO, &rfds);
-
-    if (select(STDIN_FILENO + 1, &rfds, NULL, NULL, &tv) <= 0) {
-        return;
-    }
-
-    char line[64];
-
-    if (!fgets(line, sizeof(line), stdin)) {
-        return;
-    }
-
-    char dir;
-    int speed;
-
-    if (line[0] == 'S' || line[0] == 's') {
-        current_speed = 0x80;
-        print_packet("CMD STOP");
-        return;
-    }
-
-    if (sscanf(line, " %c %d", &dir, &speed) == 2) {
-        if (speed < 2) speed = 2;
-        if (speed > 127) speed = 127;
-
-        if (dir == 'F' || dir == 'f') {
-            current_speed = 0x80 | speed;
-            print_packet("CMD FORWARD");
-        }
-        else if (dir == 'R' || dir == 'r') {
-            current_speed = speed;
-            print_packet("CMD REVERSE");
-        }
-    }
+    // End
+    send_bit(pio, sm, 1, one_count, zero_count);
 }
 
 int main() {
-    setvbuf(stdout, NULL, _IONBF, 0);
     stdio_init_all();
 
     struct gpiod_chip *chip = gpiod_chip_open("/dev/gpiochip0");
-    if (!chip) {
-        printf("Failed to open gpiochip0\n");
-        return 1;
-    }
-
     struct gpiod_line *en_line = gpiod_chip_get_line(chip, PIN_EN);
-    if (!en_line) {
-        printf("Failed to get EN line\n");
-        return 1;
-    }
-
-    if (gpiod_line_request_output(en_line, "dcc", 1) < 0) {
-        printf("Failed to enable L298 EN\n");
-        return 1;
-    }
+    gpiod_line_request_output(en_line, "dcc", 1);
 
     PIO pio = pio0;
     int sm = pio_claim_unused_sm(pio, true);
@@ -154,24 +79,19 @@ int main() {
     pio_sm_set_consecutive_pindirs(pio, sm, PIN_A, 2, true);
 
     pio_sm_config c = dcc_wave_program_get_default_config(offset);
-
     sm_config_set_sideset_pins(&c, PIN_A);
     sm_config_set_clkdiv(&c, 125.0f);
 
     pio_sm_init(pio, sm, offset, &c);
     pio_sm_set_enabled(pio, sm, true);
 
-    printf("DCC Controller Ready\n");
-    printf("Commands: F <speed>, R <speed>, S\n");
-    print_packet("START");
+    uint32_t one_count  = pio_count_from_us(DCC_ONE_US);
+    uint32_t zero_count = pio_count_from_us(DCC_ZERO_US);
+
+    printf("Sending DCC IDLE packet ONLY\n");
 
     while (1) {
-        handle_stdin_command();
-        build_packet();
-
-        for (int i = 0; i < wave_count; i++) {
-            pio_sm_put_blocking(pio, sm, wave[i]);
-        }
+        send_idle(pio, sm, one_count, zero_count);
     }
 
     return 0;
