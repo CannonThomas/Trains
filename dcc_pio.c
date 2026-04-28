44e71b7dae2d2d841f6ca27e7314b30fdb4708cf
Cannon Thomas <cannonthomas@Cannons-MacBook-Pro.local>
2026-04-28 07:05:07 -0500

    up

--- dcc_pio.c
+++ dcc_pio.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <sched.h>
-#include <sys/mman.h>
 #include <gpiod.h>
 
 #include "pico/stdlib.h"
@@ -10,78 +8,89 @@
 #include "dcc_wave.pio.h"
 
 #define PIN_EN 18
-#define PIN_A  23   // L298 IN1
-#define PIN_B  24   // L298 IN2
+#define PIN_A  23   // IN1
+#define PIN_B  24   // IN2
 
 #define DCC_ONE_US   58
 #define DCC_ZERO_US 116
 
 #define LOCO_ADDR 3
-
 #define DCC_INST_128 0x3F
 #define DCC_SPEED    0x94   // forward speed 20
 #define DCC_CHECKSUM (LOCO_ADDR ^ DCC_INST_128 ^ DCC_SPEED)
 
-#define MAX_HALVES 1024
-
-static uint32_t wave[MAX_HALVES];
-static int wave_count = 0;
-
 static inline uint32_t pio_count_from_us(uint32_t us) {
-    return (us > 2) ? (us - 2) : 1;
+    if (us > 2) return us - 2;
+    return 1;
 }
 
-static inline void add_bit(int bit) {
-    uint32_t count = bit ? pio_count_from_us(DCC_ONE_US)
-                         : pio_count_from_us(DCC_ZERO_US);
+static inline void send_bit(PIO pio, int sm, int bit,
+                            uint32_t one_count,
+                            uint32_t zero_count) {
+    uint32_t count = bit ? one_count : zero_count;
 
-    wave[wave_count++] = count;
-    wave[wave_count++] = count;
+    // One DCC bit = two opposite half cycles
+    pio_sm_put_blocking(pio, sm, count);
+    pio_sm_put_blocking(pio, sm, count);
 }
 
-static void add_byte(uint8_t b) {
+static void send_byte(PIO pio, int sm, uint8_t b,
+                      uint32_t one_count,
+                      uint32_t zero_count) {
     for (int i = 7; i >= 0; i--) {
-        add_bit((b >> i) & 1);
+        send_bit(pio, sm, (b >> i) & 1, one_count, zero_count);
     }
 }
 
-static void build_packet(void) {
-    wave_count = 0;
-
+static void send_packet(PIO pio, int sm,
+                        uint32_t one_count,
+                        uint32_t zero_count) {
     // Preamble
     for (int i = 0; i < 20; i++) {
-        add_bit(1);
+        send_bit(pio, sm, 1, one_count, zero_count);
     }
 
-    add_bit(0);
-    add_byte(LOCO_ADDR);
+    // 0 + address
+    send_bit(pio, sm, 0, one_count, zero_count);
+    send_byte(pio, sm, LOCO_ADDR, one_count, zero_count);
 
-    add_bit(0);
-    add_byte(DCC_INST_128);
+    // 0 + instruction
+    send_bit(pio, sm, 0, one_count, zero_count);
+    send_byte(pio, sm, DCC_INST_128, one_count, zero_count);
 
-    add_bit(0);
-    add_byte(DCC_SPEED);
+    // 0 + speed
+    send_bit(pio, sm, 0, one_count, zero_count);
+    send_byte(pio, sm, DCC_SPEED, one_count, zero_count);
 
-    add_bit(0);
-    add_byte(DCC_CHECKSUM);
+    // 0 + checksum
+    send_bit(pio, sm, 0, one_count, zero_count);
+    send_byte(pio, sm, DCC_CHECKSUM, one_count, zero_count);
 
-    add_bit(1);
-}
-
-static void set_realtime_priority(void) {
-    struct sched_param param;
-    param.sched_priority = 80;
-    sched_setscheduler(0, SCHED_FIFO, &param);
-    mlockall(MCL_CURRENT | MCL_FUTURE);
+    // End bit
+    send_bit(pio, sm, 1, one_count, zero_count);
 }
 
 int main() {
     stdio_init_all();
-    set_realtime_priority();
 
     struct gpiod_chip *chip = gpiod_chip_open("/dev/gpiochip0");
+    if (!chip) {
+        printf("Failed to open gpiochip0\n");
+        return 1;
+    }
+
     struct gpiod_line *en_line = gpiod_chip_get_line(chip, PIN_EN);
-    gpiod_line_request_output(en_line, "dcc", 1);
+    if (!en_line) {
+        printf("Failed to get EN line\n");
+        gpiod_chip_close(chip);
+        return 1;
+    }
+
+    if (gpiod_line_request_output(en_line, "dcc", 1) < 0) {
+        printf("Failed to enable EN\n");
+        gpiod_chip_close(chip);
+        return 1;
+    }
 
     PIO pio = pio0;
     int sm = pio_claim_unused_sm(pio, true);
@@ -93,23 +102,24 @@ int main() {
 
     pio_sm_config c = dcc_wave_program_get_default_config(offset);
 
-    // SET pins (not side-set)
-    sm_config_set_set_pins(&c, PIN_A, 2);
+    // Side-set controls GPIO23 and GPIO24
+    sm_config_set_sideset_pins(&c, PIN_A);
 
-    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_TX);
+    // 125 MHz / 125 = 1 MHz
     sm_config_set_clkdiv(&c, 125.0f);
 
     pio_sm_init(pio, sm, offset, &c);
     pio_sm_set_enabled(pio, sm, true);
 
-    build_packet();
+    uint32_t one_count  = pio_count_from_us(DCC_ONE_US);
+    uint32_t zero_count = pio_count_from_us(DCC_ZERO_US);
 
-    printf("Sending DCC: 03 3F 94 A8\n");
+    printf("Sending Bachmann DCC packet: %02X %02X %02X %02X\n",
+           LOCO_ADDR, DCC_INST_128, DCC_SPEED, DCC_CHECKSUM);
+    printf("one_count=%u zero_count=%u\n", one_count, zero_count);
 
     while (1) {
-        for (int i = 0; i < wave_count; i++) {
-            pio_sm_put_blocking(pio, sm, wave[i]);
-        }
+        send_packet(pio, sm, one_count, zero_count);
     }
 
     return 0;
