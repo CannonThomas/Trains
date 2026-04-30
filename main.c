bdd5980da42c9611f20603af4977130166973530
Cannon Thomas <cannonthomas@Cannons-MacBook-Pro.local>
2026-04-30 02:13:01 -0500

    o

--- main.c
+++ main.c
@@ -2,103 +2,140 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/select.h>
 
-#include "piolib.h"
-#include "hardware/pio_instructions.h"
+#include "hardware/pio.h"
 #include <lgpio.h>
+
+#define GPIOCHIP 0
 
 #define PIN_ENA 18
 #define PIN_IN2 23
 #define PIN_IN1 24
 
 #define LOCO_ADDR 3
+
 #define DCC_ONE_US 58
 #define DCC_ZERO_US 100
 
-#define GPIOCHIP 0
-
 static int gpio_handle = -1;
-static uint32_t txbuf[512];
-static int txlen = 0;
-static uint8_t data_byte = 0x60;
+static volatile bool running = true;
 static bool track_on = false;
+static uint8_t data_byte = 0x60;
 
-static inline uint16_t ss(uint value)
+/*
+ * Raw PIO instructions for:
+ *
+ * .program dcc_wave
+ * .side_set 2
+ *
+ * pull block        side 0b01
+ * mov x, osr        side 0b01
+ * loop_a:
+ * jmp x-- loop_a    side 0b01
+ *
+ * pull block        side 0b10
+ * mov x, osr        side 0b10
+ * loop_b:
+ * jmp x-- loop_b    side 0b10
+ *
+ * side 0b01 = GPIO23 high, GPIO24 low
+ * side 0b10 = GPIO23 low, GPIO24 high
+ */
+static const uint16_t dcc_wave_program_instructions[] = {
+    0x88a0,
+    0xa827,
+    0x0842,
+    0x90a0,
+    0xb027,
+    0x1045
+};
+
+static const struct pio_program dcc_wave_program = {
+    .instructions = dcc_wave_program_instructions,
+    .length = 6,
+    .origin = -1
+};
+
+static void handle_sigint(int sig)
 {
-    return pio_encode_sideset(2, value);
-}
-
-static void build_pio_program(uint16_t instr[6])
-{
-    instr[0] = pio_encode_pull(false, true) | ss(0b01);
-    instr[1] = pio_encode_mov(pio_x, pio_osr) | ss(0b01);
-    instr[2] = pio_encode_jmp_x_dec(2) | ss(0b01);
-
-    instr[3] = pio_encode_pull(false, true) | ss(0b10);
-    instr[4] = pio_encode_mov(pio_x, pio_osr) | ss(0b10);
-    instr[5] = pio_encode_jmp_x_dec(5) | ss(0b10);
+    (void)sig;
+    running = false;
 }
 
 static uint32_t count_from_us(uint32_t us)
 {
+    /*
+     * Each half-cycle has:
+     * pull + mov + loop overhead
+     * so subtract a small offset.
+     */
     return us > 3 ? us - 3 : 1;
 }
 
-static void add_half(uint32_t count)
+static void send_half(PIO pio, unsigned int sm, uint32_t count)
 {
-    if (txlen < 512)
-    {
-        txbuf[txlen++] = count;
-    }
+    pio_sm_put_blocking(pio, sm, count);
 }
 
-static void add_bit(int bit, uint32_t one_count, uint32_t zero_count)
+static void send_bit(PIO pio, unsigned int sm, int bit,
+                     uint32_t one_count,
+                     uint32_t zero_count)
 {
     uint32_t count = bit ? one_count : zero_count;
-    add_half(count);
-    add_half(count);
+
+    // First half polarity: IN2=1 IN1=0
+    send_half(pio, sm, count);
+
+    // Second half polarity: IN2=0 IN1=1
+    send_half(pio, sm, count);
 }
 
-static void add_byte(uint8_t b, uint32_t one_count, uint32_t zero_count)
+static void send_byte(PIO pio, unsigned int sm, uint8_t b,
+                      uint32_t one_count,
+                      uint32_t zero_count)
 {
     for (int i = 7; i >= 0; i--)
     {
-        add_bit((b >> i) & 1, one_count, zero_count);
+        send_bit(pio, sm, (b >> i) & 1, one_count, zero_count);
     }
 }
 
-static int build_packet(uint8_t addr, uint8_t data,
+static void send_packet(PIO pio, unsigned int sm,
+                        uint8_t addr,
+                        uint8_t data,
                         uint32_t one_count,
                         uint32_t zero_count)
 {
     uint8_t checksum = addr ^ data;
 
-    txlen = 0;
-
+    // Preamble
     for (int i = 0; i < 14; i++)
     {
-        add_bit(1, one_count, zero_count);
+        send_bit(pio, sm, 1, one_count, zero_count);
     }
 
-    add_bit(0, one_count, zero_count);
-    add_byte(addr, one_count, zero_count);
+    send_bit(pio, sm, 0, one_count, zero_count);
+    send_byte(pio, sm, addr, one_count, zero_count);
 
-    add_bit(0, one_count, zero_count);
-    add_byte(data, one_count, zero_count);
+    send_bit(pio, sm, 0, one_count, zero_count);
+    send_byte(pio, sm, data, one_count, zero_count);
 
-    add_bit(0, one_count, zero_count);
-    add_byte(checksum, one_count, zero_count);
+    send_bit(pio, sm, 0, one_count, zero_count);
+    send_byte(pio, sm, checksum, one_count, zero_count);
 
-    add_bit(1, one_count, zero_count);
-
-    return txlen * sizeof(uint32_t);
+    send_bit(pio, sm, 1, one_count, zero_count);
 }
 
 static void set_track(bool on)
 {
     track_on = on;
-    lgGpioWrite(gpio_handle, PIN_ENA, on ? 1 : 0);
+
+    if (gpio_handle >= 0)
+    {
+        lgGpioWrite(gpio_handle, PIN_ENA, on ? 1 : 0);
+    }
 }
 
 static void handle_keyboard(void)
@@ -136,12 +173,16 @@ static void handle_keyboard(void)
     {
         data_byte = 0x60;
         set_track(true);
-        printf("\nSTOP: 03 60 63\n");
+        printf("\nSTOP PACKETS: 03 60 63\n");
     }
     else if (c == 'x' || c == 'X')
     {
         set_track(false);
         printf("\nTRACK OFF\n");
+    }
+    else if (c == 'q' || c == 'Q')
+    {
+        running = false;
     }
 
     fflush(stdout);
@@ -149,7 +190,7 @@ static void handle_keyboard(void)
 
 int main(void)
 {
-    uint16_t instructions[6];
+    signal(SIGINT, handle_sigint);
 
     gpio_handle = lgGpiochipOpen(GPIOCHIP);
     if (gpio_handle < 0)
@@ -158,36 +199,31 @@ int main(void)
         return 1;
     }
 
-    lgGpioClaimOutput(gpio_handle, 0, PIN_ENA, 0);
-
-    build_pio_program(instructions);
-
-    struct pio_program dcc_program = {
-        .instructions = instructions,
-        .length = 6,
-        .origin = 0
-    };
+    if (lgGpioClaimOutput(gpio_handle, 0, PIN_ENA, 0) < 0)
+    {
+        printf("Failed to claim ENA GPIO%d\n", PIN_ENA);
+        return 1;
+    }
 
     PIO pio = pio0;
-    int sm = pio_claim_unused_sm(pio, true);
-
-    pio_sm_config_xfer(pio, sm, PIO_DIR_TO_SM, sizeof(txbuf), 1);
-
-    uint offset = pio_add_program(pio, &dcc_program);
+    unsigned int sm = pio_claim_unused_sm(pio, true);
+    unsigned int offset = pio_add_program(pio, &dcc_wave_program);
 
     pio_gpio_init(pio, PIN_IN2);
     pio_gpio_init(pio, PIN_IN1);
     pio_sm_set_consecutive_pindirs(pio, sm, PIN_IN2, 2, true);
 
-    pio_sm_config c = pio_get_default_sm_config();
+    pio_sm_config config = pio_get_default_sm_config();
 
-    sm_config_set_wrap(&c, offset + 0, offset + 5);
-    sm_config_set_sideset(&c, 2, false, false);
-    sm_config_set_sideset_pins(&c, PIN_IN2);
-    sm_config_set_clkdiv(&c, 125.0f);
+    sm_config_set_wrap(&config, offset + 0, offset + 5);
+    sm_config_set_sideset(&config, 2, false, false);
+    sm_config_set_sideset_pins(&config, PIN_IN2);
+
+    // Target ~1 MHz PIO timing. Adjust after scope if needed.
+    sm_config_set_clkdiv(&config, 125.0f);
 
     pio_sm_clear_fifos(pio, sm);
-    pio_sm_init(pio, sm, offset, &c);
+    pio_sm_init(pio, sm, offset, &config);
     pio_sm_set_enabled(pio, sm, true);
 
     uint32_t one_count = count_from_us(DCC_ONE_US);
@@ -197,16 +233,16 @@ int main(void)
     printf("GPIO18 = ENA\n");
     printf("GPIO23 = IN2\n");
     printf("GPIO24 = IN1\n");
-    printf("Commands: f r s x\n");
+    printf("Commands: f r s x q\n");
+    printf("Start with scope only. Type f to begin packets.\n");
 
-    while (1)
+    while (running)
     {
         handle_keyboard();
 
         if (track_on)
         {
-            int bytes = build_packet(LOCO_ADDR, data_byte, one_count, zero_count);
-            pio_sm_xfer_data(pio, sm, PIO_DIR_TO_SM, bytes, (uint8_t *)txbuf);
+            send_packet(pio, sm, LOCO_ADDR, data_byte, one_count, zero_count);
         }
         else
         {
@@ -214,5 +250,13 @@ int main(void)
         }
     }
 
+    set_track(false);
+
+    if (gpio_handle >= 0)
+    {
+        lgGpiochipClose(gpio_handle);
+    }
+
+    printf("\nStopped.\n");
     return 0;
 }
