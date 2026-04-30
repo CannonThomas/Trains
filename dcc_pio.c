8f7787652774e432f24cc59081bb327336200a05
Cannon Thomas <cannonthomas@Cannons-MacBook-Pro.local>
2026-04-29 23:43:54 -0500

    ok

--- dcc_pio.c
+++ dcc_pio.c
@@ -3,27 +3,28 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <sys/select.h>
-#include <gpiod.h>
+#include <lgpio.h>
 
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "dcc_wave.pio.h"
 
-#define PIN_EN 18
-#define PIN_A  23   // IN2
-#define PIN_B  24   // IN1
+#define GPIOCHIP 0
+
+#define ENA 18
+#define PIN_A 23   // IN2
+#define PIN_B 24   // IN1
 
 #define DCC_ONE_US   58
-#define DCC_ZERO_US 100   // matches old group method
+#define DCC_ZERO_US 100
 
 #define LOCO_ADDR 3
 
-static struct gpiod_line *en_line = NULL;
-static uint8_t data_byte = 0x60;   // STOP packet data
-static bool track_on = false;
+static int h;
+static uint8_t data_byte = 0x60; // STOP packet
 
 static inline uint32_t pio_count_from_us(uint32_t us) {
-    return (us > 2) ? (us - 2) : 1;
+    return (us > 2) ? us - 2 : 1;
 }
 
 static inline void send_bit(PIO pio, int sm, int bit,
@@ -48,10 +49,7 @@ static void send_packet(PIO pio, int sm,
                         uint32_t zero_count) {
     uint8_t checksum = LOCO_ADDR ^ data_byte;
 
-    // Old group used 12 preamble 1s: 8 + 4
-    for (int i = 0; i < 12; i++) {
-        send_bit(pio, sm, 1, one_count, zero_count);
-    }
+    for (int i = 0; i < 12; i++) send_bit(pio, sm, 1, one_count, zero_count);
 
     send_bit(pio, sm, 0, one_count, zero_count);
     send_byte(pio, sm, LOCO_ADDR, one_count, zero_count);
@@ -65,19 +63,7 @@ static void send_packet(PIO pio, int sm,
     send_bit(pio, sm, 1, one_count, zero_count);
 }
 
-static void track_enable(PIO pio, int sm) {
-    gpiod_line_set_value(en_line, 1);
-    pio_sm_set_enabled(pio, sm, true);
-    track_on = true;
-}
-
-static void track_disable(PIO pio, int sm) {
-    gpiod_line_set_value(en_line, 0);
-    pio_sm_set_enabled(pio, sm, false);
-    track_on = false;
-}
-
-static void handle_cmd(PIO pio, int sm) {
+static void handle_cmd(void) {
     fd_set rfds;
     struct timeval tv = {0, 0};
 
@@ -91,23 +77,23 @@ static void handle_cmd(PIO pio, int sm) {
 
     if (line[0] == 'F' || line[0] == 'f') {
         data_byte = 0x63;
-        track_enable(pio, sm);
+        lgGpioWrite(h, ENA, 1);
         printf("FORWARD | packet 03 63 60\n");
     }
     else if (line[0] == 'R' || line[0] == 'r') {
         data_byte = 0x43;
-        track_enable(pio, sm);
+        lgGpioWrite(h, ENA, 1);
         printf("REVERSE | packet 03 43 40\n");
     }
     else if (line[0] == 'S' || line[0] == 's') {
         data_byte = 0x60;
-        track_disable(pio, sm);
-        printf("STOP | packet 03 60 63 | ENA OFF | PIO OFF\n");
+        lgGpioWrite(h, ENA, 1);   // keep DCC alive, send stop packet
+        printf("STOP PACKETS | packet 03 60 63 | ENA ON\n");
     }
-    else if (line[0] == 'I' || line[0] == 'i') {
+    else if (line[0] == 'X' || line[0] == 'x') {
         data_byte = 0x60;
-        track_enable(pio, sm);
-        printf("IDLE/STOP PACKETS ON | packet 03 60 63\n");
+        lgGpioWrite(h, ENA, 0);   // emergency kill
+        printf("EMERGENCY OFF | ENA OFF\n");
     }
 
     fflush(stdout);
@@ -117,29 +103,20 @@ int main() {
     stdio_init_all();
     setvbuf(stdout, NULL, _IONBF, 0);
 
-    struct gpiod_chip *chip = gpiod_chip_open("/dev/gpiochip0");
-    if (!chip) {
-        printf("Failed to open gpiochip0\n");
+    h = lgGpiochipOpen(GPIOCHIP);
+    if (h < 0) {
+        printf("Failed to open gpiochip\n");
         return 1;
     }
 
-    en_line = gpiod_chip_get_line(chip, PIN_EN);
-    if (!en_line) {
-        printf("Failed to get EN line\n");
-        return 1;
-    }
-
-    if (gpiod_line_request_output(en_line, "dcc_enable", 0) < 0) {
-        printf("Failed to request EN line\n");
-        return 1;
-    }
+    lgGpioClaimOutput(h, 0, ENA, 1); // ENA always on for DCC
 
     PIO pio = pio0;
     int sm = pio_claim_unused_sm(pio, true);
     uint offset = pio_add_program(pio, &dcc_wave_program);
 
-    pio_gpio_init(pio, PIN_A);
-    pio_gpio_init(pio, PIN_B);
+    pio_gpio_init(pio, PIN_A); // GPIO23 / IN2
+    pio_gpio_init(pio, PIN_B); // GPIO24 / IN1
     pio_sm_set_consecutive_pindirs(pio, sm, PIN_A, 2, true);
 
     pio_sm_config c = dcc_wave_program_get_default_config(offset);
@@ -147,28 +124,20 @@ int main() {
     sm_config_set_clkdiv(&c, 125.0f);
 
     pio_sm_init(pio, sm, offset, &c);
-    pio_sm_set_enabled(pio, sm, false);
+    pio_sm_set_enabled(pio, sm, true);
 
     uint32_t one_count  = pio_count_from_us(DCC_ONE_US);
     uint32_t zero_count = pio_count_from_us(DCC_ZERO_US);
 
-    printf("DCC ready - old group packet mode\n");
+    printf("DCC PIO with verified polarity logic\n");
     printf("GPIO18=ENA, GPIO23=IN2, GPIO24=IN1\n");
-    printf("Commands: F, R, S, I\n");
-    printf("F = 03 63 60\n");
-    printf("R = 03 43 40\n");
-    printf("S = 03 60 63 with ENA OFF\n");
-    printf("I = 03 60 63 continuously with ENA ON\n");
+    printf("Commands: F, R, S, X\n");
 
     while (1) {
-        handle_cmd(pio, sm);
-
-        if (track_on) {
-            send_packet(pio, sm, one_count, zero_count);
-        } else {
-            usleep(1000);
-        }
+        handle_cmd();
+        send_packet(pio, sm, one_count, zero_count);
     }
 
+    lgGpiochipClose(h);
     return 0;
 }
