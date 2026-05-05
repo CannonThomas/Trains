f2e44e0d6d30331fe023365ddcf3f8663dbf3644
Cannon Thomas <cannonthomas@Cannons-MacBook-Pro.local>
2026-05-05 14:27:40 -0500

    upd sec

--- TrainController.py
+++ TrainController.py
@@ -600,7 +600,7 @@ class TrainController:
         SPEED = FWD_SPEED  # legacy alias for FWD-only contexts
         LOCO  = train_config.LOCO_NAME
         # Per-track REV durations on pickup
-        PICKUP_REV_SEC = {1: 9.0, 2: 10.0, 3: 12.0}
+        PICKUP_REV_SEC = {1: 9.0, 2: 12.0, 3: 15.0}
 
         try:
             self.io.set_all_straight()
